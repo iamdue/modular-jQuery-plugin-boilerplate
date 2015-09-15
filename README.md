8942914b7efb756ef858aeb5fac650ca34b23856
Sagi Gyorgy <sagi.gyorgy@w5.hu>
2015-09-15 20:03:31 +0200

    Added requireJS compatible jquery plugin boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # modular_jQuery_plugin_boilerplate
-Boilerplate for writing modular jQuery plugins. (requireJS, commonJS, etc…)
+Boilerplate for writing modular jQuery plugins. (requireJS, commonJS, etc…).
+This plugin boilerplate frame is successfully used is several projects that use modular async js loading with requireJS.
+
+Contribute
+----------
+If you feel that you can help improve the code, then feel free to contact me to contribute on this boilerplate.
+
+Author
+------
+Gyorgy Sagi (gyorgy.sagi[at]gmail.com)
---
+++ plugin.js
@@ -0,0 +1,80 @@
+/**
+ * modular jQuery plugin boilerplate
+ * @version 1.0
+ * @author Gyorgy Sagi <gyorgy.sagi@gmail.com>
+ * @desc dropCrossZone is an implementation of a dropZone file uploader for crosssec
+ */
+
+'use strict';
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// CommonJS
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory( root.jQuery );
+	}
+}
+(this, function( $ ){
+
+	var defaults = {
+		defaultProperties: {}
+	};
+
+	/**
+	 * PluginName constructor
+	 * @constructs PluginName object
+	 * @param element - initiated element(s)
+	 * @param options - configuration options
+	 */
+	function PluginName( element, options ) {
+		this.element = element;
+
+		this.options = $.extend( {}, defaults, options) ;
+
+		this._defaults = defaults;
+
+		this.init();
+
+	}
+
+	/**
+	 * extends PluginName object prototype
+	 * @type {{init: Function, addEventHandlers: Function}}
+	 */
+	PluginName.prototype = {
+
+		init: function(){
+			this.addEventHandlers();
+		},
+
+		/**
+		 * add Eventhandlers
+		 */
+		addEventHandlers: function(){
+
+		}
+
+	};
+
+	/**
+	 * PluginName function
+	 * @desc extending jQuery object with PluginName function
+	 * @param options
+	 * @returns {*}
+	 */
+	$.fn.PluginName = function ( options ) {
+
+		return this.each(function() {
+			if ( !$.data( this, "plugin_PluginName" ) ) {
+				$.data( this, "plugin_PluginName", new PluginName( this, options ) );
+			}
+		});
+
+	};
+
+}));
